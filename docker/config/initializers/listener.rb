30b916b5d3c24265ad372f2c89228fce8f399020
Josh Larsen <jlarsen@gmail.com>
2020-10-09 19:57:00 -0400

    setup control file watcher

---
+++ docker/config/initializers/listener.rb
@@ -0,0 +1,20 @@
+#
+# Reload Controls on changes to `opencspm-controls`
+#
+CORE_CONTAINER = 'core'.freeze
+LISTEN_DIR = File.join(Rails.root, 'controls')
+logger = Rails.logger
+
+# Only listen for changes on core container
+if File.directory?(LISTEN_DIR) && Socket.gethostname == CORE_CONTAINER
+  listener = Listen.to(LISTEN_DIR) do |modified, added, removed|
+    logger.debug("modified absolute path: #{modified}")
+    logger.debug("added absolute path: #{added}")
+    logger.debug("removed absolute path: #{removed}")
+  end
+
+  # non blocking
+  listener.start
+
+  puts "=> [listener] listening for changes to #{LISTEN_DIR}"
+end
