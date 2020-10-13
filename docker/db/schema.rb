6c3e3e99f7fee4d3bfce581733272415920a3ed8
Josh Larsen <jlarsen@gmail.com>
2020-10-13 16:57:26 -0400

    re-write schema.rb

--- docker/db/schema.rb
+++ docker/db/schema.rb
@@ -10,169 +10,170 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_201_013_202_815) do
+ActiveRecord::Schema.define(version: 2020_10_13_202815) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'campaigns', force: :cascade do |t|
-    t.string 'name'
-    t.bigint 'organization_id', null: false
-    t.bigint 'user_id', null: false
-    t.jsonb 'filters'
-    t.integer 'control_count', default: 0
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['name'], name: 'index_campaigns_on_name'
-    t.index ['organization_id'], name: 'index_campaigns_on_organization_id'
-    t.index ['user_id'], name: 'index_campaigns_on_user_id'
+  create_table "campaigns", force: :cascade do |t|
+    t.string "name"
+    t.bigint "organization_id", null: false
+    t.bigint "user_id", null: false
+    t.jsonb "filters"
+    t.integer "control_count", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_campaigns_on_name"
+    t.index ["organization_id"], name: "index_campaigns_on_organization_id"
+    t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
 
-  create_table 'controls', force: :cascade do |t|
-    t.string 'guid'
-    t.string 'control_pack'
-    t.string 'control_id'
-    t.string 'title'
-    t.text 'description'
-    t.string 'platform'
-    t.integer 'impact'
-    t.string 'validation'
-    t.string 'remediation'
-    t.jsonb 'refs'
-    t.integer 'status', default: 0
-    t.integer 'resources_failed', default: 0
-    t.integer 'resources_total', default: 0
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['control_id'], name: 'index_controls_on_control_id'
-    t.index ['control_pack'], name: 'index_controls_on_control_pack'
-    t.index ['guid'], name: 'index_controls_on_guid'
-    t.index ['platform'], name: 'index_controls_on_platform'
-    t.index ['title'], name: 'index_controls_on_title'
+  create_table "controls", force: :cascade do |t|
+    t.string "guid"
+    t.string "control_pack"
+    t.string "control_id"
+    t.string "title"
+    t.text "description"
+    t.string "platform"
+    t.integer "impact"
+    t.string "validation"
+    t.string "remediation"
+    t.jsonb "refs"
+    t.integer "status", default: 0
+    t.integer "resources_failed", default: 0
+    t.integer "resources_total", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["control_id"], name: "index_controls_on_control_id"
+    t.index ["control_pack"], name: "index_controls_on_control_pack"
+    t.index ["guid"], name: "index_controls_on_guid"
+    t.index ["platform"], name: "index_controls_on_platform"
+    t.index ["title"], name: "index_controls_on_title"
   end
 
-  create_table 'identities', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.string 'provider'
-    t.string 'uid'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_identities_on_user_id'
+  create_table "identities", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
-  create_table 'issues', force: :cascade do |t|
-    t.integer 'status'
-    t.bigint 'result_id', null: false
-    t.bigint 'resource_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['resource_id'], name: 'index_issues_on_resource_id'
-    t.index ['result_id'], name: 'index_issues_on_result_id'
+  create_table "issues", force: :cascade do |t|
+    t.integer "status"
+    t.bigint "result_id", null: false
+    t.bigint "resource_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["resource_id"], name: "index_issues_on_resource_id"
+    t.index ["result_id"], name: "index_issues_on_result_id"
   end
 
-  create_table 'jobs', force: :cascade do |t|
-    t.integer 'status'
-    t.integer 'issue_count'
-    t.integer 'kind'
-    t.string 'guid'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['guid'], name: 'index_jobs_on_guid'
+  create_table "jobs", force: :cascade do |t|
+    t.integer "status"
+    t.integer "issue_count"
+    t.integer "kind"
+    t.string "guid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["guid"], name: "index_jobs_on_guid"
   end
 
-  create_table 'organizations', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['name'], name: 'index_organizations_on_name'
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_organizations_on_name"
   end
 
-  create_table 'profiles', force: :cascade do |t|
-    t.string 'name'
-    t.string 'author'
-    t.string 'platform'
-    t.jsonb 'tags'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "profiles", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.string "platform"
+    t.jsonb "tags"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'resources', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['name'], name: 'index_resources_on_name'
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_resources_on_name"
   end
 
-  create_table 'results', force: :cascade do |t|
-    t.bigint 'job_id', null: false
-    t.jsonb 'data'
-    t.bigint 'control_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.datetime 'observed_at', precision: 6
-    t.index ['control_id'], name: 'index_results_on_control_id'
-    t.index ['job_id'], name: 'index_results_on_job_id'
+  create_table "results", force: :cascade do |t|
+    t.bigint "job_id", null: false
+    t.jsonb "data"
+    t.bigint "control_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "observed_at", precision: 6
+    t.index ["control_id"], name: "index_results_on_control_id"
+    t.index ["job_id"], name: "index_results_on_job_id"
   end
 
-  create_table 'roles', force: :cascade do |t|
-    t.string 'name'
-    t.string 'resource_type'
-    t.bigint 'resource_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index %w[name resource_type resource_id], name: 'index_roles_on_name_and_resource_type_and_resource_id'
-    t.index %w[resource_type resource_id], name: 'index_roles_on_resource_type_and_resource_id'
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
-  create_table 'sources', force: :cascade do |t|
-    t.string 'name'
-    t.jsonb 'datasources'
-    t.integer 'status'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "sources", force: :cascade do |t|
+    t.string "name"
+    t.jsonb "datasources"
+    t.integer "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'taggings', force: :cascade do |t|
-    t.bigint 'tag_id', null: false
-    t.bigint 'control_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['control_id'], name: 'index_taggings_on_control_id'
-    t.index ['tag_id'], name: 'index_taggings_on_tag_id'
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "control_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["control_id"], name: "index_taggings_on_control_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
-  create_table 'tags', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.bigint 'organization_id', null: false
-    t.string 'username', default: '', null: false
-    t.string 'password_digest', default: '', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['organization_id'], name: 'index_users_on_organization_id'
-    t.index ['username'], name: 'index_users_on_username', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.bigint "organization_id", null: false
+    t.string "username", default: "", null: false
+    t.string "password_digest", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organization_id"], name: "index_users_on_organization_id"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
-  create_table 'users_roles', id: false, force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'role_id'
-    t.index ['role_id'], name: 'index_users_roles_on_role_id'
-    t.index %w[user_id role_id], name: 'index_users_roles_on_user_id_and_role_id'
-    t.index ['user_id'], name: 'index_users_roles_on_user_id'
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
-  add_foreign_key 'campaigns', 'organizations'
-  add_foreign_key 'campaigns', 'users'
-  add_foreign_key 'identities', 'users'
-  add_foreign_key 'issues', 'resources'
-  add_foreign_key 'issues', 'results'
-  add_foreign_key 'results', 'controls'
-  add_foreign_key 'results', 'jobs'
-  add_foreign_key 'taggings', 'controls'
-  add_foreign_key 'taggings', 'tags'
-  add_foreign_key 'users', 'organizations'
+  add_foreign_key "campaigns", "organizations"
+  add_foreign_key "campaigns", "users"
+  add_foreign_key "identities", "users"
+  add_foreign_key "issues", "resources"
+  add_foreign_key "issues", "results"
+  add_foreign_key "results", "controls"
+  add_foreign_key "results", "jobs"
+  add_foreign_key "taggings", "controls"
+  add_foreign_key "taggings", "tags"
+  add_foreign_key "users", "organizations"
 end
