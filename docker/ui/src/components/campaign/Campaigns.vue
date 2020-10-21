8151df8ac852bcaeecf36fb8a95926c6ae07b531
Josh Larsen <jlarsen@gmail.com>
2020-10-21 13:04:36 -0400

    alignment

--- docker/ui/src/components/campaign/Campaigns.vue
+++ docker/ui/src/components/campaign/Campaigns.vue
@@ -43,8 +43,8 @@
                          viewBox="0 0 20 20"
                          fill="currentColor">
                       <path fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd" />
+                            clip-rule="evenodd"
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" />
                     </svg>
                   </button>
                 </div>
@@ -67,8 +67,8 @@
                     Controls
                   </th>
                   <th
-                      class="hidden md:table-cell px-6 py-3 border-b border-gray-200 bg-gray-50 text-right text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    Created
+                      class="table-cell px-6 py-3 whitespace-no-wrap border-b border-gray-200 bg-gray-50 text-right text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Last Changed
                   </th>
                 </tr>
               </thead>
@@ -89,11 +89,11 @@
                   <td class="px-6 py-3 text-sm leading-5 text-gray-500 font-medium">
                     <div class="flex items-center space-x-2">
                       <span
-                            class="flex-shrink-0 text-xs leading-5 font-medium">{{ campaign.control_count }}</span>
+                            class="mx-auto flex-shrink-0 text-xs text-center leading-5 font-medium">{{ campaign.control_count }}</span>
                     </div>
                   </td>
                   <td
-                      class="hidden md:table-cell px-6 py-3 whitespace-no-wrap text-sm leading-5 text-gray-500 text-right">
+                      class="table-cell px-6 py-3 whitespace-no-wrap text-sm leading-5 text-gray-500 text-right">
                     {{ campaign.updated_at | moment }}
                   </td>
                 </tr>
