8ffa3023e52ee4e744d4165ba4863dc3da5080cc
Josh Larsen <jlarsen@gmail.com>
2020-10-27 21:45:10 -0400

    more services

---
+++ docker/lib/rg/aws_loader/elbv2.rb
@@ -0,0 +1,16 @@
+#
+# Load ElasticLoadBalancingV2 assets into RedisGraph
+#
+# Each method returns an array of Cypher queries
+#
+class AWSLoader::ElasticLoadBalancingV2 < AwsGraphDbLoader
+  def load_balancer
+    node = 'AWS_ELASTIC_LOAD_BALANCER'
+    q = []
+
+    # load_balancer node
+    q.push(_upsert({ node: node, id: @name }))
+
+    q
+  end
+end
