8eccc15ca49b41e22daf0c376da96ce5f33ff5c4
Josh Larsen <jlarsen@gmail.com>
2020-11-12 10:38:55 -0500

    remove Enterprise notice

--- docker/ui/src/components/control/ControlModal.vue
+++ docker/ui/src/components/control/ControlModal.vue
@@ -46,32 +46,6 @@
           <Tag v-for="(tag, idx) in data.tags.filter(x => x.primary ).sort((a,b) => a.tag.localeCompare(b.tag) )"
                :key=idx>{{ tag.tag }}</Tag>
         </div>
-        <div v-if="isEnterpriseControl"
-             class="mx-2">
-          <div class="bg-yellow-50 border-l-4 border-yellow-400 p-4">
-            <div class="flex">
-              <div class="flex-shrink-0">
-                <svg class="h-5 w-5 text-yellow-400"
-                     viewBox="0 0 20 20"
-                     fill="currentColor">
-                  <path fill-rule="evenodd"
-                        d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
-                        clip-rule="evenodd" />
-                </svg>
-              </div>
-              <div class="ml-3">
-                <p class="text-sm leading-5 text-yellow-700">
-                  This control is part of an Enterprise Control Pack.
-                  <a href="https://darkbit.io/opencspm/subscribe"
-                     class="font-medium underline text-yellow-700 hover:text-yellow-600 transition ease-in-out duration-150">
-                    Learn how to upgrade.
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-
-        </div>
         <div v-if="data.tags && data.tags.filter(x => !x.primary).length > 0"
              class="mx-2 pb-4">
           <div @click="toggleSecondaryTags"
