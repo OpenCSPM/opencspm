b3a79b7d0d238d1550db08196a7062de33c6be83
Josh Larsen <jlarsen@gmail.com>
2020-11-19 10:53:00 -0500

    add smoke test GHA

--- docker/app/controllers/healthcheck_controller.rb
+++ docker/app/controllers/healthcheck_controller.rb
@@ -8,4 +8,21 @@ class HealthcheckController < ApplicationController
   def index
     render plain: 'ok', status: :ok
   end
+
+  #
+  # Respond to smoke test status check
+  #
+  def show
+    statuses = [
+      Job.count,
+      Resource.count,
+      Issue.count,
+      Control.count
+    ]
+
+    status = statuses.all? { |s| s > 0 }
+    response_code = status ? :ok : :service_unavailable
+
+    render plain: response_code.to_s, status: response_code
+  end
 end
