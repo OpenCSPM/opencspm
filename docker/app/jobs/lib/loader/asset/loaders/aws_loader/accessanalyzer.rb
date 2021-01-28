da7e8a5cc9bb601d005bedd2a5a49aa5bafbd6f3
Josh Larsen <jlarsen@gmail.com>
2021-01-28 16:02:08 -0500

    load Access Analyzer and Service Quotas

--- docker/app/jobs/lib/loader/asset/loaders/aws_loader/accessanalyzer.rb
+++ docker/app/jobs/lib/loader/asset/loaders/aws_loader/accessanalyzer.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
+#
+# Load AccessAnalyzer assets into RedisGraph
+#
+# Each method returns an array of Cypher queries
+#
 class AWSLoader::AccessAnalyzer < GraphDbLoader
+  def analyzer
+    node = 'AWS_ACCESS_ANALYZER'
+    q = []
+
+    # analyzer node
+    q.push(_upsert({ node: node, id: @name }))
+  end
 end
--- docker/app/jobs/lib/loader/asset/loaders/aws_loader/servicequotas.rb
+++ docker/app/jobs/lib/loader/asset/loaders/aws_loader/servicequotas.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
+#
+# Load ServiceQuota assets into RedisGraph
+#
+# Each method returns an array of Cypher queries
+#
 class AWSLoader::ServiceQuotas < GraphDbLoader
+  def quota
+    node = 'AWS_SERVICE_QUOTA'
+    q = []
+
+    # quota node
+    q.push(_upsert({ node: node, id: @name }))
+  end
 end
