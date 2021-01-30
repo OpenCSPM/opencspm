bf581ac67373ba4b6791aa7b6a80942faa651ccc
Josh Larsen <jlarsen@gmail.com>
2021-01-30 17:57:57 -0500

    load more Elastic Search domain attributes

--- docker/app/jobs/lib/loader/asset/loaders/aws_loader/elasticsearch.rb
+++ docker/app/jobs/lib/loader/asset/loaders/aws_loader/elasticsearch.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #
 # Load ElasticsearchService assets into RedisGraph
 #
@@ -10,6 +12,22 @@ class AWSLoader::ElasticsearchService < GraphDbLoader
 
     # domain node
     q.push(_upsert({ node: node, id: @name }))
+
+    # additional attributes
+    enforce_https = @data&.domain_endpoint_options&.enforce_https || false
+    encryption_at_rest = @data&.encryption_at_rest_options&.enabled || false
+    node_to_node = @data&.node_to_node_encryption_options&.enabled || false
+    log_publishing_options = @data&.log_publishing_options&.to_h&.keys&.join(',')
+    cognito_options = @data&.cognito_options&.enabled || false
+
+    q.push(_append({ node: node, id: @name,
+                     data: {
+                       enforce_https_enabled: enforce_https,
+                       encryption_at_rest_enabled: encryption_at_rest,
+                       node_to_node_encryption_options: node_to_node,
+                       log_publishing_options: log_publishing_options,
+                       cognito_options_enabled: cognito_options
+                     }}))
 
     q
   end
