05dfb1d060f542995d499ac8d08bb6892c971283
Josh Larsen <jlarsen@gmail.com>
2021-02-03 16:48:25 -0500

    load GuardDuty detector findings statistics

--- docker/app/jobs/lib/loader/asset/loaders/aws_loader/guardduty.rb
+++ docker/app/jobs/lib/loader/asset/loaders/aws_loader/guardduty.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 #
 # Load GuardDuty assets into RedisGraph
 #
@@ -10,6 +12,13 @@ class AWSLoader::GuardDuty < GraphDbLoader
 
     # detector node
     q.push(_upsert({ node: node, id: @name }))
+
+    # detector findings statistics
+    q.push(_append({ node: node, id: @name, data: {
+                     findings: @data&.findings_statistics&.count_by_severity&.to_h&.values&.inject(:+) || 0,
+                     findings_aged_short: @data&.findings_statistics_aged_short&.count_by_severity&.to_h&.values&.inject(:+) || 0,
+                     findings_aged_long: @data&.findings_statistics_aged_long&.count_by_severity&.to_h&.values&.inject(:+) || 0
+                   } }))
 
     q
   end
