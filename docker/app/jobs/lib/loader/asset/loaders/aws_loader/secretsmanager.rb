5914003547e10250ab82346178fe8a3132097cb2
Josh Larsen <jlarsen@gmail.com>
2021-02-16 10:03:13 -0500

    scaffold loader for AWSLoader::SecretsManager

---
+++ docker/app/jobs/lib/loader/asset/loaders/aws_loader/secretsmanager.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+#
+# Load SecretsManager assets into RedisGraph
+#
+# Each method returns an array of Cypher queries
+#
+class AWSLoader::SecretsManager < GraphDbLoader
+end
