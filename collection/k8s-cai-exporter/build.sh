b82b14a98b39894f76f94496b60fbb315a1184f8
Brad Geesaman <bradgeesaman+github@gmail.com>
2021-03-12 18:05:13 -0500

    update gke exporter image

--- collection/k8s-cai-exporter/build.sh
+++ collection/k8s-cai-exporter/build.sh
@@ -5,3 +5,8 @@ TAG="$(cat version)"
 
 gcloud config set project opencspm
 gcloud builds submit --tag "gcr.io/${IMAGE}:${TAG}"
+
+IMAGE="darkbit-io/gke-exporter"
+
+gcloud config set project darkbit-io
+gcloud builds submit --tag "gcr.io/${IMAGE}:${TAG}"
