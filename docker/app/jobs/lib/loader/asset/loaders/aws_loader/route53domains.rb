2e3de800da2c1fed6974ba3f9b4fdb9caba2ee56
Josh Larsen <jlarsen@gmail.com>
2021-04-26 09:34:59 -0400

    Route53Domains loader scaffold

---
+++ docker/app/jobs/lib/loader/asset/loaders/aws_loader/route53domains.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+#
+# Load Route53Domains assets into RedisGraph
+#
+# Each method returns an array of Cypher queries
+#
+class AWSLoader::Route53Domains < GraphDbLoader
+end
